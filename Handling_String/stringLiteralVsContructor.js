a813725e238a12745d724ac94dab6f1adc742cae
Maruf-Bellah <mdmaruf5511507@gmail.com>
2023-07-09 11:57:28 +0600

    first commit

---
+++ Handling_String/stringLiteralVsContructor.js
@@ -0,0 +1,10 @@
+// sing / dubble contetion এর মধ্যে text দিয়ে যদি কোন এক variable মধ্যে assing করা হয় তাহলে তাকে string বলে 
+
+
+var str = 'Something'; // somethin এটা কে বলে literal 
+var str2 = String('Something') //এটা কে বলে string constructor
+
+
+var n = 10;
+var str3 = String(n);
+console.log(str3);
