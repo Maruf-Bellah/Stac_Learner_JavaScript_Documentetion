b3716999c144239060255439c75f6188a3b218d9
Maruf-Bellah <mdmaruf5511507@gmail.com>
2023-07-12 00:45:12 +0600

    after three days i push code in my document

--- Handling_String_4/stringLiteralVsContructor.js
+++ Handling_String_4/stringLiteralVsContructor.js
@@ -2,9 +2,13 @@
 
 
 var str = 'Something'; // somethin এটা কে বলে literal 
-var str2 = String('Something') //এটা কে বলে string constructor
+var str2 = String('maruf bellah') //এটা কে বলে string constructor
+ 
+console.log(str2);
 
 
 var n = 10;
+var str5 = n + ' ' //যে কোন নাম্বার কে যদি plass sing দিয়ে concat করা হয় তাহলে সেটা string হয়ে যায় 
+var str6 = n.toString();
 var str3 = String(n);
 console.log(str3);
